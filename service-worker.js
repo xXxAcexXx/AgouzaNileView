74b439d1176f3ccc19c8e927023a276292734ce8
Ahmed Essam <essam_3276@hotmail.com>
2023-05-24 15:04:44 +0300

    live

--- service-worker.js
+++ service-worker.js
@@ -38,98 +38,70 @@ var cacheFiles = [
 	'/images/nigeria.webp',
 	'/images/Saudi_arabia_flag_large.webp',
 	'/images/whatsapp.webp',
-	// Include paths to other images you want to cache...
+	// Your images here...
 ];
-
 
 self.addEventListener('install', function(e) {
     console.log('[ServiceWorker] Installed');
 
-    // e.waitUntil Delays the event until the Promise is resolved
     e.waitUntil(
-
-    	// Open the cache
 	    caches.open(cacheName).then(function(cache) {
-
-	    	// Add all the default files to the cache
 			console.log('[ServiceWorker] Caching cacheFiles');
 			return cache.addAll(cacheFiles);
 	    })
-	); // end e.waitUntil
+	);
 });
-
 
 self.addEventListener('activate', function(e) {
     console.log('[ServiceWorker] Activated');
 
     e.waitUntil(
-
-    	// Get all the cache keys (cacheName)
 		caches.keys().then(function(cacheNames) {
 			return Promise.all(cacheNames.map(function(thisCacheName) {
-
-				// If a cached item is saved under a previous cacheName
 				if (thisCacheName !== cacheName) {
-
-					// Delete that cached file
 					console.log('[ServiceWorker] Removing Cached Files from Cache - ', thisCacheName);
 					return caches.delete(thisCacheName);
 				}
 			}));
 		})
-	); // end e.waitUntil
+	);
 });
-
 
 self.addEventListener('fetch', function(e) {
 	console.log('[ServiceWorker] Fetch', e.request.url);
+	
+	// If the request does not target an image, skip it
+	if (!e.request.url.match(/(jpe?g|png|gif|svg|webp)$/)) {
+		console.log("[ServiceWorker] Non-image Fetch, skipping...", e.request.url);
+		return;
+	}
 
-	// e.respondWith Responds to the fetch event
 	e.respondWith(
-
-		// Check in cache for the request being made
 		caches.match(e.request)
-
 			.then(function(response) {
-
-				// If the request is in the cache
-				if ( response ) {
+				if (response) {
 					console.log("[ServiceWorker] Found in Cache", e.request.url, response);
-					// Return the cached version
 					return response;
 				}
 
-				// If the request is NOT in the cache, fetch and cache
-
-				var requestClone = e.request.clone();
-				fetch(requestClone)
+				return fetch(e.request.clone())
 					.then(function(response) {
-
-						if ( !response ) {
-							console.log("[ServiceWorker] No response from fetch ")
+						if (!response || response.status !== 200) {
 							return response;
 						}
 
 						var responseClone = response.clone();
-
-						//  Open the cache
 						caches.open(cacheName).then(function(cache) {
-
-							// Put the fetched response in the cache
 							cache.put(e.request, responseClone);
 							console.log('[ServiceWorker] New Data Cached', e.request.url);
+						});
 
-							// Return the response
-							return response;
-			        
-				        }); // end caches.open
-
+						return response;
 					})
 					.catch(function(err) {
 						console.log('[ServiceWorker] Error Fetching & Caching New Data', err);
 					});
-
-
-			}) // end caches.match(e.request)
-	); // end e.respondWith
+			})
+	);
 });
+
